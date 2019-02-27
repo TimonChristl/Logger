652608f9876e0d83f8422032b19060d51099b0fe
Timon Christl <timonchristl@gmail.com>
2019-02-27 21:13:32 +0100

    Migrated to .NET Standard 2.0

--- src/TC.Logging/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("TC.Logging")]
-[assembly: AssemblyDescription("Timon Christl's logging library")]
-[assembly: ComVisible(false)]
-[assembly: Guid("3ED27A87-BA91-4269-B649-C1D8AB7F37CA")]
